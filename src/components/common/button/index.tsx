f4ad2bb4a96886ed4825bc9cabb3c4ef082a0018
kyechan99 <kyechan99@gmail.com>
2023-12-16 21:25:32 +0900

    feat: Storybook: react-vite

---
+++ src/components/common/button/index.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  font-size: 1rem;
+  font-weight: 600;
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+  outline: none;
+
+  .icon.icon-sm {
+    margin-right: 0.25rem;
+  }
+  .icon.icon-md {
+    margin-right: 0.5rem;
+  }
+`;
+export default Button;
---
+++ src/stories/common/Button.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from '@/components/common/button';
+
+const meta = {
+  title: 'common/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {},
+  parameters: {
+    layout: 'fullscreen',
+    componentSubtitle: 'Base Button',
+    docs: {
+      description: {
+        component: `Base Button of Everything.`,
+      },
+    },
+  },
+} satisfies Meta<React.ComponentProps<'button'>>;
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {},
+};
+
+export const Secondary: Story = {
+  args: {
+    ...Primary.args,
+  },
+};
